67f6ab220c9053e1e6195976d0a6df41c5b16d17
mina-yousry <mina.yousry.samir@gmail.com>
2018-06-01 18:32:27 +0200

    Friday 1/6/2018 Mina OfferedCourses fragment almost done

--- app/src/main/java/inc/talentedinc/API/APIUrls.java
+++ app/src/main/java/inc/talentedinc/API/APIUrls.java
@@ -10,13 +10,14 @@ package inc.talentedinc.API;
 public class APIUrls {
 
     public static final String API_KEY="005f422fa64ad9f31cd41964b94b2cf3";
-    public static final String BASE_URL="http://api.themoviedb.org";
+    public static final String BASE_URL="https://www.themoviedb.org/";
     public static final String UPCOMING="/3/discover/movie";
     public static final String SEARCH_UPCOMING="/3/search/movie";
     public static final String HISTORY="/3/discover/movie";
 
     //alaa
     public static final String USER_LOGIN = "/api/users";
+    public static final String CREATE_COURSE = "/api/createcourse";
 
 
 
