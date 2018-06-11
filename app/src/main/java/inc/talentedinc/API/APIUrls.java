0978ade8312bf073de207e36be4ed382ce4b4267
AlaaMuhamed <35137692+AlaaMuhamed@users.noreply.github.com>
2018-06-11 18:53:01 +0200

    alaa commit workspace final

--- app/src/main/java/inc/talentedinc/API/APIUrls.java
+++ app/src/main/java/inc/talentedinc/API/APIUrls.java
@@ -12,7 +12,7 @@ public class APIUrls {
     public static final int SUCCESS =200;
     public static final String API_KEY = "005f422fa64ad9f31cd41964b94b2cf3";
 
-    public static final String BASE_URL="https://tal-company.herokuapp.com";
+//    public static final String BASE_URL="https://tal-company.herokuapp.com";
     public static final String DISCOVER_MOV3IES="/3/discover/movie";
     public static final String SEARCH_MOVIES="/3/search/movie";
 
